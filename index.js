d578076bf30045e8531d3f165642bc5c4ba0cca7
Tanaroeg Ocharoen <graph_koy@hotmail.com>
2022-07-09 07:39:33 +0700

    Edit cors

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const Code = require('./models/Code')
 const Answer = require('./models/Answer')
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 mongoose.connect('mongodb+srv://tanaroeg123:70iAIxB4kbJpnUYT@synerr.0bhhwiu.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true
@@ -61,8 +68,6 @@ app.post('/api/answer/verify', async (req, res) => {
         })
     }
 })
-
-app.post('/api')
 
 app.listen(port, () => {
     console.log("Listen on port: " + port);
